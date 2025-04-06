97d4db050db45afbb53e40b4f0db6573c4b1f75b
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-04-06 14:16:52 -0400

    Add `markdown_link_check` and `readme_reference_links_are_sorted` tests

--- tests/ci.rs
+++ tests/ci.rs
@@ -1,4 +1,6 @@
 use assert_cmd::Command;
+use regex::Regex;
+use std::{fs::read_to_string, path::Path};
 
 #[test]
 fn clippy() {
@@ -38,4 +40,36 @@ fn hack_feature_powerset_udeps() {
         ])
         .assert()
         .success();
+}
+
+#[test]
+fn markdown_link_check() {
+    let tempdir = tempfile::tempdir().unwrap();
+
+    Command::new("npm")
+        .args(["install", "markdown-link-check"])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+
+    let readme_md = Path::new(env!("CARGO_MANIFEST_DIR")).join("README.md");
+
+    Command::new("npx")
+        .args(["markdown-link-check", &readme_md.to_string_lossy()])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+}
+
+#[test]
+fn readme_reference_links_are_sorted() {
+    let re = Regex::new(r"^\[[^\]]*\]:").unwrap();
+    let readme = read_to_string("README.md").unwrap();
+    let links = readme
+        .lines()
+        .filter(|line| re.is_match(line))
+        .collect::<Vec<_>>();
+    let mut links_sorted = links.clone();
+    links_sorted.sort_unstable();
+    assert_eq!(links_sorted, links);
 }
