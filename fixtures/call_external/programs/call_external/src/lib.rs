677aa3370dca081eef195161727c213d974720fc
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-04-06 13:35:59 -0400

    Add `include_cargo_does_not_change_line_hits` test

---
+++ fixtures/call_external/programs/call_external/src/lib.rs
@@ -0,0 +1,20 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod call_external {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let signer = &ctx.accounts.signer;
+        let pubkey = signer.signer_key().unwrap();
+        msg!("Signer's pubkey is: {}", pubkey);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub signer: Signer<'info>,
+}
