c1f3afe5285fc6869e4064a9907bc561c845a999
Samuel Moelius <sam@moeli.us>
2025-05-18 20:15:25 -0400

    Add `multiple_programs` fixture

---
+++ fixtures/multiple_programs/programs/foo/src/lib.rs
@@ -0,0 +1,41 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod foo {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn increment_x(ctx: Context<IncrementX>) -> Result<()> {
+        ctx.accounts.storage.x += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = payer, space = 8 + Storage::INIT_SPACE, seeds = [], bump)]
+    pub storage: Account<'info, Storage>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct IncrementX<'info> {
+    #[account(mut, seeds = [], bump)]
+    pub storage: Account<'info, Storage>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Storage {
+    x: u64,
+}
---
+++ fixtures/multiple_programs/tests/bar.ts
@@ -0,0 +1,30 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { Bar } from "../target/types/bar";
+import * as assert from "assert";
+
+describe("bar", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.bar as Program<Bar>;
+
+  const [storage_address] = PublicKey.findProgramAddressSync(
+    [Buffer.from("")],
+    program.programId,
+  );
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Should increment `y`", async () => {
+    const before = await program.account.storage.fetch(storage_address);
+    await program.methods.incrementY().rpc();
+    const after = await program.account.storage.fetch(storage_address);
+    assert.ok(before.y.addn(1).eq(after.y));
+  });
+});
