36884781bdaa49bd4963e6bc16ae2740144578d3
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-09-04 12:34:37 -0400

    Do not allow Dylint to cause nested workspaces to be built

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+use std::env::var_os;
+
 fn main() {
-    nested_workspace::build().unwrap();
+    // smoelius: Do not allow Dylint to cause the nested workspaces to be built. Doing so can cause
+    // "incompatible version of rustc" errors.
+    if var_os("RUSTC_WORKSPACE_WRAPPER").is_none() {
+        nested_workspace::build().unwrap();
+    }
 }
